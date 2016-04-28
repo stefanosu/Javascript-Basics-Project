ee711917ef63593c4ac9e1364f000ccdf30c744e
Stefanos Ugbit <stefanosu422@gmail.com>
2016-04-27 23:23:52 -0400

    put comments in the Javscript functions and commented out the frm.email.focus lines to see if there were any possible changes when the code runs.

--- Javascript Basics Project/scripts.js
+++ Javascript Basics Project/scripts.js
@@ -1,23 +1,26 @@
-//test comment
+//NOTE: Not sure what frm.email.focus does. I commented it out to see if there were any changes but could not find any.
+
+//if the text field is empty then give  the alert and return false
 function val () {
+
 	if(frm.email.value =="") {
 		
 		alert("Please enter the email");
-		frm.email.focus();
+		//frm.email.focus();
 		return false;
  }
 
-
+//gives all possible values for what a valid email could look like
 var reg = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
 
+//if our input doesnt qualify as something that could equal to reg, show the alert and return FALSE, otherwise return TRUE and take user to the next page.
 if (reg.test(frm.email.value) == false) {
 
 	alert('Invalid email address');
-	frm.email.focus();
+	//frm.email.focus();
 	return false;
 } 
 	return true;
 }
 
-window.open('mailto:test@example.com?subject=subject&body=body');
 
